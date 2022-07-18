e8b2da190f694a1390cf7757ba371bc393e22037
pponnek <rbudi6823@gmail.com>
2022-07-18 07:00:19 +0700

    portofolio tailwinds

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+       center: true,
+       padding: '16px'
+    },
+    extend: {
+      colors:{
+        primary: '#22c55e',
+        dark: '#064e3b',
+      },
+      screens:{
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
